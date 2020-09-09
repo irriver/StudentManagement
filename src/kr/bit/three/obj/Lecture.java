b3864699424780b36c928eddb6369870b6fa4fff
irriver <66699849+irriver@users.noreply.github.com>
2020-09-09 14:59:17 +0900

    Revert "develop1.1"

--- src/kr/bit/three/obj/Lecture.java
+++ src/kr/bit/three/obj/Lecture.java
@@ -3,37 +3,12 @@ import java.io.Serializable;
 
 public class Lecture implements Serializable {
 	
-	private String lecCode;
 	private String lecName;
-	private String professor;
-	private String semester;
+	private String lecCode;
+	private String prof;
 	private int times;
-	private int	numOfStd;
+	private String semester;
 	private int maxStd;
-	
-	@Override
-	public String toString() {
-		return "Lecture [lecCOde=" + lecCode + ", lecName=" + lecName + ", professor=" + professor + ", semester="
-				+ semester + ", times=" + times + ", maxStd=" + maxStd + "]";
-	}
-
-	
-	public void addNumOfStd() {
-		if(numOfStd < maxStd) {
-			numOfStd++;
-		} else {
-			System.out.println("수강인원을 초과했습니다. 다른 강의를 신청해주세요.");
-		}
-	}
-	
-	public void subNumOfStd() {
-		numOfStd--;
-	}
-	
-	//getter, setter
-	public String getLecCode() {
-		return lecCode;
-	}
 	
 	
 }
