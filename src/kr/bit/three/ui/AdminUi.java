9b551457d5fa8b5c31e066507215c22ea07c106f
5racoon <nafe0@naver.com>
2020-09-12 23:07:48 +0900

    etc : ui, uicontroller 수정
    강사님이 사용하시는 폰트에 맞춰 ui, uicontroller 디자인을 수정하였다.

--- src/kr/bit/three/ui/AdminUi.java
+++ src/kr/bit/three/ui/AdminUi.java
@@ -7,75 +7,72 @@ public class AdminUi {
 	Scanner scan = new Scanner(System.in);
 	
 	public String adminUi() {
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("        비트 대학교 학사관리 시스템(관리자)");
-		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("    1. 학생 관리");
-		System.out.println("    2. 교수 관리");
-		System.out.println("    3. 강의 관리");
-		System.out.println("    0. 이전메뉴");
-		System.out.println();
-		System.out.println("------------------------------------");
-		System.out.printf("번호 입력 >> ");
+		System.out.println("*******************************************");
+		System.out.println("                                                                      ");
+		System.out.println("           비트대학교 학사관리 시스템(관리자)          ");
+		System.out.println("        ㅡㅡㅡㅡㅡㅡㅡ* 메뉴 *ㅡㅡㅡㅡㅡㅡ        ");
+		System.out.println("                     1.         학사 관리                           ");
+		System.out.println("                     2.         교수 관리                           ");
+		System.out.println("                     3.         강의 관리                          ");
+		System.out.println("                     4.         수강 관리                           ");
+		System.out.println("                     0.         이전 메뉴                          ");
+		System.out.println("       ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ       ");
+		System.out.println("                                                                      ");
+		System.out.println("*******************************************");
+		System.out.printf("메뉴 번호를 입력해 주세요 : ");
 		String inputNum = scan.nextLine();
 		return inputNum;
 	}
 	
 	public String manageStudent(){
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("        학생 관리");
-		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("    1. 학생정보 등록");
-		System.out.println("    2. 학생정보 수정");
-		System.out.println("    3. 전체 학생정보 조회");
-		//System.out.println("    4. 강의 수강 학생 조회");
-		System.out.println("    0. 이전메뉴");
-		System.out.println();
-		System.out.println("------------------------------------");
-		System.out.printf("번호 입력 >> ");
+		System.out.println("*******************************************");
+		System.out.println("                                                                      ");
+		System.out.println("                                학생 관리                          ");
+		System.out.println("        ㅡㅡㅡㅡㅡㅡㅡ* 메뉴 *ㅡㅡㅡㅡㅡㅡ       ");
+		System.out.println("                     1.         학사 등록                           ");
+		System.out.println("                     2.         학생 수정                          ");
+		System.out.println("                     3.         전체 학생 조회                   ");
+		System.out.println("                     4.         강의 수강 학생 조회            ");
+		System.out.println("                     0.         이전 메뉴                          ");
+		System.out.println("       ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ      ");
+		System.out.println("                                                                      ");
+		System.out.println("*******************************************");
+		System.out.printf("메뉴 번호를 입력해 주세요 : ");
 		String inputNum = scan.nextLine();
 		return inputNum;
 	}
 	
 	public String manageProfessor(){
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("        교수 관리");
-		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("    1. 교수정보 등록");
-		System.out.println("    2. 교수정보 수정");
-		System.out.println("    3. 전체 교수정보 조회");
-		System.out.println("    0. 이전메뉴");
-		System.out.println();
-		System.out.println("------------------------------------");
-		System.out.printf("번호 입력 >> ");
+		System.out.println("*******************************************");
+		System.out.println("                                                                      ");
+		System.out.println("                                교수 관리                            ");
+		System.out.println("        ㅡㅡㅡㅡㅡㅡㅡ* 메뉴 *ㅡㅡㅡㅡㅡㅡ        ");
+		System.out.println("                     1.         교수 등록                             ");
+		System.out.println("                     2.         교수 수정                            ");
+		System.out.println("                     3.         교수 등록                            ");
+		System.out.println("                     0.         이전 메뉴                          ");
+		System.out.println("       ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ      ");
+		System.out.println("                                                                      ");
+		System.out.println("********************************************");
+		System.out.printf("메뉴 번호를 입력해 주세요 : ");
 		String inputNum = scan.nextLine();
 		return inputNum;
 	}
 	
 	public String manageSubject(){
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("        강의 관리");
-		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("    1. 강의 개설");
-		System.out.println("    2. 강의 수정");
-		//System.out.println("    3. 강의 삭제");
-		System.out.println("    4. 강의 조회");
-		System.out.println("    0. 이전메뉴");
-		System.out.println();
-		System.out.println("------------------------------------");
-		System.out.printf("번호 입력 >> ");
+		System.out.println("*******************************************");
+		System.out.println("                                                                      ");
+		System.out.println("                                강의 관리                           ");
+		System.out.println("        ㅡㅡㅡㅡㅡㅡㅡ* 메뉴 *ㅡㅡㅡㅡㅡㅡ        ");
+		System.out.println("                     1.         강의 개설                           ");
+		System.out.println("                     2.         강의 수정                           ");
+		System.out.println("                     3.         강의 삭제                          ");
+		System.out.println("                     3.         강의 조회                          ");
+		System.out.println("                     0.         이전 메뉴                          ");
+		System.out.println("       ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ      ");
+		System.out.println("                                                                      ");
+		System.out.println("*******************************************");
+		System.out.printf("메뉴 번호를 입력해 주세요 : ");
 		String inputNum = scan.nextLine();
 		return inputNum;
 	}		
--- src/kr/bit/three/ui/MainUi.java
+++ src/kr/bit/three/ui/MainUi.java
@@ -7,19 +7,17 @@ public class MainUi {
 
 	public String UiMethod() {	
 		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("            비트 대학교 학사관리 시스템");
-		System.out.println();
-		System.out.println("======================");
-		System.out.println();
-		System.out.println("    1. 학생");
-		System.out.println("    2. 교수");
-		System.out.println("    3. 관리자");
-		System.out.println("    4. 종료");
-		System.out.println();
-		System.out.println("------------------------------------");
-		System.out.printf("사용자 계정을 선택해 주세요 >> ");
+		System.out.println("*******************************************");
+		System.out.println("                                                                     ");
+		System.out.println("                 비트대학교 학사관리 시스템                 ");
+		System.out.println("        ㅡㅡㅡㅡㅡㅡㅡ* 메뉴 *ㅡㅡㅡㅡㅡㅡ       ");
+		System.out.println("                     1.          학생                                 ");
+		System.out.println("                     2.          교수                                  ");
+		System.out.println("                     3.          관리자                              ");
+		System.out.println("       ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ      ");
+		System.out.println("                                                                      ");
+		System.out.println("*******************************************");
+		System.out.printf("메뉴 번호를 입력해 주세요 : ");		
 		String inputNum = scan.nextLine();
 		return inputNum;
 	}
